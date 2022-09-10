316eb556e1fa78824b41b393dfc25ddc7f41cfda
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2022-09-10 21:46:13 +0100

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/Vishal151/production-ready-api/internal/transport/http"
 )
 
 // App - the struct which contains things like pointers
@@ -11,6 +14,15 @@ type App struct{}
 // Run - handles the startup of the application
 func (app *App) Run() error {
 	fmt.Println("Setting up our application")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to start server")
+		return err
+	}
+
 	return nil
 }
 
